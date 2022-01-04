d717a7c5ff258f8fe467d50fed77cb4c7b016931
Anton Gavrilyuk <amigoalt@gmail.com>
2022-01-05 01:40:36 +0300

    Memory leaks fixed.

--- src/rules.c
+++ src/rules.c
@@ -1,78 +1,185 @@
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "rules_structure.h"
-#include "rules_method.h"
+#ifndef STRUCTURE_LOADED
+#include "rules.h"
+#endif
 
-void list_vars(Rule *rule);
-void list_rules(Module *module);
-void load_var(Variable *var, Module *module);
-
-int main()
+void add_to_cache(Variable *v, Module *m)
 {
-  printf("-------------------------------\n");
-  printf("[tsrules] service is starting..\n");
+  Cache *cache;
+  // If cache is empty
+  if(m->cache == NULL)
+  {
+    cache = NULL;
+    cache = create_cache_item(cache, v);
+    m->cache = cache;
+  }
+  else
+  {
+    cache = m->cache;
+    cache = create_cache_item(cache, v);
+    m->cache = cache;
+  }
 
+}
 
-  Source *src1, *src2, *src3;
-  //src = NULL;
-  Variable *var, *var1, *var2, *var3;
-  var = NULL;
-  Rule *rule;
-  rule = NULL;
-  Module *module;
+int total_items_in_cache(Cache *p){
+    int t = 0;
+    // пока не конец стека
+    while (p != NULL) {
+      // продвижение по списку
+      p = p->next;
+      t++;
+    }
+    return t;
+}
 
-  module = create_module("[module] Tsmodem Rules");
-
-  char *ubus[] = {"tsmodem.driver", "signal", "value"};
-  char *uci[] = {"tsmodem", "sim_1", "name"};
-  src1 = create_source("ubus", ubus);
-  src2 = create_source("uci", uci);
-  src3 = create_source("uci", uci);
-
-  rule = create_rule(rule, "[rule] Switching sim card if not registered.", module);
-
-  var1 = create_variable(var, "varname1", src1, rule);
-  var2 = create_variable(var1, "varname2", src2, rule);
-  var3 = create_variable(var2, "varname3", src2, rule);
-
-
-  printf("First-from-the-end Var name:\t%s\n", var2->name);
-  printf("Second-from-the-end Var name:\t%s\n", var2->next->name);
-
-  printf("Rule title:\t%s\n", rule->title);
-  printf("Module title\t%s\n", module->title);
-
-  list_rules(module);
-  list_vars(rule);
-
-  load_var(var1, module);
-  load_var(var2, module);
-  load_var(var3, module);
-
-  printf("TOTAL ITEMS IN CACHE: %d\n", total_items_in_cache(module->cache));
-
+int is_in_cache(Variable *var, Module *module)
+{
+  Cache *cache;
+  cache = module->cache;
+  while(cache != NULL)
+  {
+    if(strcmp(var->source.hash, cache->source.hash) == 0)
+        return 1;
+    cache = cache->next;
+  }
   return 0;
 }
 
-void list_vars(Rule *rule)
-{
-  printf("\n---- LIST OF VARS FOR [%s] ----\n", rule->title);
-  Variable *v;
-  v = rule->stack_of_vars;
-  while(v != NULL) {
-    printf("Name:\t\t%s\n", v->name);
-    v = v->next;
-  }
+void clear_cache(Cache *c){
+    int t = 0;
+    Cache *cache;
+    while (c != NULL) {
+      cache = c->next;
+      free(c);
+      c = cache;
+
+      t++;
+    }
+    printf("Cleared in cache: %d\n", t);
+
 }
 
-void list_rules(Module *module)
-{
-  printf("\n---- LIST OF RULES FOR [%s] ----\n", module->title);
-  Rule *r;
-  r = module->stack_of_rules;
-  while(r != NULL) {
-    printf("Rule name:\t\t%s\n", r->title);
-    r = r->next;
+bool is_float(const char *s) {
+  if (s == NULL) {
+    return false;
   }
+  char *endptr;
+  (float) strtod(s, &endptr);
+  if (s == endptr) {
+    return false; // no conversion;
+  }
+  // Look at trailing text
+  while (isspace((unsigned char ) *endptr))
+    endptr++;
+  return *endptr == '\0';
+}
+
+// Loads initial data to variable 'subtotal' field.
+void load_var(Variable *var, Module *module)
+{
+  char response[256];
+  long int result_i;
+
+  if(strcmp(var->source.type, "uci") == 0)
+  {
+    // TODO get from UCI code here
+    // and place to cache
+    strcpy(response, "250.70");
+    strcpy(var->source.x.uci_response, response);
+  }
+  else if(strcmp(var->source.type, "ubus") == 0)
+  {
+
+    // TODO get from UBUS code here
+    // and place to cache
+
+    strcpy(response, "-500.10");
+    strcpy(var->source.x.ubus_response, response);
+  }
+
+  else
+  {
+    // for constants
+    strcpy(response, var->input.x.string);
+    var->input.type = STRING;
+  }
+
+
+  if(strlen(response) == 0)
+  {
+    var->input.x.string = strcpy(var->input.x.string, "");
+    var->input.type = STRING;
+  }
+  else // convert to REAL or INTEGER or STRING
+  {
+      if(strcmp(response, "0") == 0)
+      {
+          var->input.x.integer = 0;
+          var->input.type = INTEGER;
+      }
+      else
+      {
+          char *endp;
+          result_i = (long int) strtol(response, &endp, 10);
+          if(strlen(endp) == 0)
+          {
+              var->input.x.integer = result_i;
+              var->input.type = INTEGER;
+          }
+          else
+          {
+              if(is_float(response))
+              {
+                var->input.x.real = (float) strtod(response, NULL);
+                var->input.type = REAL;
+              }
+              else
+              {
+                  var->input.x.string = response;
+                  var->input.type = STRING;
+              }
+          }
+      }
+
+
+
+    // Check if the same value already loaded
+
+    if(is_in_cache(var, module) == 0)
+    {
+      add_to_cache(var, module);
+    }
+
+
+  }
+
+
+
+  switch(var->input.type) {
+    case STRING:
+        printf("STRING\n");
+        printf("\nVariable [%s] was loaded. Input: %s\n", var->name, var->input.x.string);
+        break;
+    case REAL:
+        printf("REAL\n");
+        printf("\nVariable [%s] was loaded. Input: %f\n", var->name, var->input.x.real);
+        break;
+    case INTEGER:
+        printf("INTEGER\n");
+        printf("\nVariable [%s] was loaded. Input: %ld\n", var->name, var->input.x.integer);
+        break;
+    default:
+        printf("--- type not defined -- \n");
+  }
+
+
 }
--- src/rules.h
+++ src/rules.h
@@ -1,20 +1,61 @@
+#ifndef HELLO_H
+#define HELLO_H 1
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define STRUCTURE_LOADED 1
 
 typedef struct Source
 {
-  char model[64];
-  char method[64];
-  char param[64];
+  char type[10];
+  char hash[64];
+  union {
+    struct {
+      char name[64];
+      char method[64];
+      char param[64];
+      char ubus_response[256];
+    };
+    struct {
+      char config[64];
+      char section[64];
+      char option[64];
+      char uci_response[64];
+    };
+  } x;
 } Source;
+
+typedef enum { INTEGER, STRING, REAL } Type;
+
+// Good explanation of Union:
+// https://stackoverflow.com/questions/724617/examples-of-union-in-c
+typedef struct Value
+{
+  Type type;
+  union
+  {
+    char *string;
+    long int integer;
+    float real;
+  } x;
+} Value;
+
+typedef struct Cache {
+  struct Source source;
+  struct Value value;
+  struct Cache *next;
+} Cache;
+
+extern Cache global_cache;
+
 
 typedef struct Variable
 {
   char name[64];
   struct Source source;
-  char input[256];
-  char output[256];
+  struct Value input;
+  struct Value subtotal;
+  struct Value output;
   struct Variable *next;
 } Variable;
 
@@ -22,56 +63,110 @@ typedef struct Rule
 {
   char title[128];
   struct Rule *next;
-  struct Variable *first_var_in_list;
+  struct Variable *stack_of_vars;
 } Rule;
 
 typedef struct Module
 {
   char title[128];
-  struct Rule *first_rule_in_list;
+  struct Rule *stack_of_rules;
+  struct Cache *cache;
 
 } Module;
 
+
 //------------------------
 
-struct Source *create_source(char *model, char *method, char *param)
+struct Source *create_source(char *model, char *array[])
 {
   Source *src;
   src = (Source *)malloc(sizeof(Source));
 
-  strcpy(src->model, model);
-  strcpy(src->method, method);
-  strcpy(src->param, param);
+  if(strcmp(model, "uci") == 0)
+  {
+    strcpy(src->type, "uci");
+
+    strcpy(src->x.config, array[0]);
+    strcpy(src->x.section, array[1]);
+    strcpy(src->x.option, array[2]);
+    strcpy(src->x.uci_response, "");
+
+    // simple hash analogue
+    // to find in cache quickly
+
+    strcpy(src->hash, src->x.config);
+    strcat(src->hash, src->x.section);
+    strcat(src->hash, src->x.option);
+
+  }
+
+
+  else if(strcmp(model, "ubus") == 0)
+  {
+    strcpy(src->type, "ubus");
+    strcpy(src->x.name, array[0]);
+    strcpy(src->x.method, array[1]);
+    strcpy(src->x.param, array[2]);
+    strcpy(src->x.ubus_response, "");
+
+    strcpy(src->hash, src->x.name);
+    strcat(src->hash, src->x.method);
+    strcat(src->hash, src->x.param);
+  }
+
+  else if(strcmp(model, "constant") == 0)
+  {
+      ;
+  }
   return src;
 }
 
-struct Variable *create_variable(Variable *v, char *name, Source src, char *input, char *output)
-{
-  Variable *var;
-  var = (Variable *)malloc(sizeof(Variable));
-  var->source = src;
-  var->next = v;
-  strcpy(var->name, name);
-  strcpy(var->input, input);
-  strcpy(var->output, output);
-  return var;
-}
-
-struct Rule *create_rule(Rule *r, char *title, Variable *first_var)
+struct Rule *create_rule(Rule *r, char *title, Module *module)
 {
   Rule *rule;
   rule = (Rule *)malloc(sizeof(Rule));
   rule->next = r;
-  rule->first_var_in_list = first_var;
+  rule->stack_of_vars = NULL;
   strcpy(rule->title, title);
+
+  module->stack_of_rules = rule;
   return rule;
 }
 
-struct Module *create_module(char *title, Rule *first_rule)
+struct Variable *create_variable(Variable *v, char *name, Source *src, Rule *rule)
+{
+  Variable *var;
+  var = (Variable *)malloc(sizeof(Variable));
+  var->source = *src;
+  var->next = v;
+  strcpy(var->name, name);
+  rule->stack_of_vars = var;
+  return var;
+}
+
+struct Module *create_module(char *title)
 {
   Module *module;
   module = (Module *)malloc(sizeof(Module));
   strcpy(module->title, title);
-  module->first_rule_in_list = first_rule;
+  module->stack_of_rules = NULL;
+  module->cache = NULL;
   return module;
 }
+
+struct Cache *create_cache_item(Cache *c, Variable *v)
+{
+  Cache *cache;
+  cache = (Cache *)malloc(sizeof(Cache));
+  cache->source = v->source;
+  cache->value = v->input;
+  cache->next = c;
+  return cache;
+}
+
+extern void add_to_cache(Variable *v, Module *m);
+extern int total_items_in_cache(Cache *p);
+extern int is_in_cache(Variable *var, Module *module);
+extern void load_var(Variable *var, Module *module);
+
+#endif /* !HELLO_H */
